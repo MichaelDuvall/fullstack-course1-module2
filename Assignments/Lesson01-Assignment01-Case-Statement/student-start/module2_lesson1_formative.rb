94d4339979b6a493ec3b8953ccc3702bce688ccd
Michael Duvall <michaelpduvall@gmail.com>
2015-11-20 14:50:38 -0500

    Update module2_lesson1_formative.rb

--- Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
+++ Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -1,15 +1,19 @@
+#.nil? is true if the object is nil
 some_var = "false"
 another_var = "nil"
 
-if some_var == "pink elephant"
+case some_var
+
+when "pink elephant" #because some_var is equal to the string false
   puts "Don't think about the pink elephant!"
 
-elsif another_var.nil?
-  puts "Question mark in the method name?"
+when another_var.nil? # because another_var is a string
+  puts "Question mark in the method name?"  
 
-elsif some_var == false
-  puts "Looks like this one should execute"
+when false #because some_var is a string
+  puts "Looks like this one should execute" 
 
 else
-  puts "I guess nothing matched... But why?"
+  puts "I guess nothing matched... But Why?"
+
 end
